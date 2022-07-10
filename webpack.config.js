514c93efd32c3960a777bf330e1321051e5b8d41
n-yuki1117 <nishitaylor1928@gmail.com>
2022-07-10 14:02:27 +0900

    all

--- webpack.config.js
+++ webpack.config.js
@@ -12,13 +12,16 @@ module.exports ={
     module: {
         rules: [
             {
-                test: /\.css/,
+                test: /\.(css|sass|scss)/,
                 use: [
                     {
                         loader: MiniCssExtractPlugin.loader,
                     },
                     {
                         loader: 'css-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
                     },
                 ],
             },
@@ -60,6 +63,10 @@ module.exports ={
         new HtmlwebpackPlugin({
             template: './src/templates/access.pug',
             filename: 'access.html',
+        }),
+        new HtmlwebpackPlugin({
+            template: './src/templates/members/taro.pug',
+            filename: 'members/taro.html',
         }),
         new CleanWebpackPlugin(),
     ],
